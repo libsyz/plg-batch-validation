b30150c61f3e66585992540279d4382dd3d62526
Miguel Jimenez <denkatalabeno@gmail.com>
2025-04-21 10:58:06 +0200

    kickstarting repo

---
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,4 @@
+AZURE_API_KEY="13iv57Cj6CZN3D8o0xVG2Amxer0hlAayo37genq531Pw6iTu7BPiJQQJ99BCACYeBjFXJ3w3AAABACOGQQj1"
+AZURE_API_VERSION="2025-01-01-preview"
+AZURE_ENDPOINT="https://validationtest.openai.azure.com/openai/files?api-version=2025-01-01-preview"
+AZURE_MODEL_NAME="gpt-4o-batch-processing"
